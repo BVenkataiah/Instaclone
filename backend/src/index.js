c4605fd7fe1570edbe50e71cb6cdbbce6d56b30b
BVenkataiah <venky151917@gmail.com>
2023-04-12 12:03:07 +0530

    files added

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 let port = process.env.PORT || 3000
 require('dotenv/config');
 
-const db = mongoose.connect(process.env.MONGOATLAS_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connect('mongodb+srv://BVenkataiah:Venky18@cluster0.tojee2p.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => console.log("connected to Database"))
             .catch((err) => console.log(err));
 

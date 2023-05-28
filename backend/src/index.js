9adeb1913b92ab60c70ad988bd9db1d287ba85fb
BVenkataiah <venky151917@gmail.com>
2023-05-28 17:20:28 +0530

    files added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,13 @@
-// for connecting to db and establishing server connection
-
-const app = require('./app')
+const express = require("express");
 const mongoose = require("mongoose");
-let port = process.env.PORT || 3000
-require('dotenv/config');
+const router = require("./routes/route");
+require('dotenv').config();
+const app = express();
+app.use(express.json());
+app.use('/', router);
 
 const db = mongoose.connect('mongodb+srv://BVenkataiah:Venky18@cluster0.tojee2p.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => console.log("connected to Database"))
             .catch((err) => console.log(err));
 
-app.listen(port, () => console.log('Server running......'));
+app.listen(5000, () => console.log('Server running......'));
